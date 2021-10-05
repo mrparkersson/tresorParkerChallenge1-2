eff3af1037db8b8bcad2a45ee5b6fb4a270a3098
mrparkersson <amoparkerprince78@gmail.com>
2021-10-05 18:08:57 +0000

    final push

--- challenge2.js
+++ challenge2.js
@@ -1,16 +1,16 @@
 function catAndMouse(x, y, z) {
-    let distBtnMouseAndCatA = Math.abs(x - z);
-     let distBtnMouseAndCatB = Math.abs(y - z);
+  let distBtnMouseAndCatA = Math.abs(x - z);
+  let distBtnMouseAndCatB = Math.abs(y - z);
 
-    if (distBtnMouseAndCatA < distBtnMouseAndCatB) {
-      console.log("Cat A")
+  if (distBtnMouseAndCatA < distBtnMouseAndCatB) {
+    console.log("Cat A");
     return "Cat A";
   }
   if (distBtnMouseAndCatA > distBtnMouseAndCatB) {
-      console.log("Cat B")
+    console.log("Cat B");
     return "Cat B";
   }
-  console.log("Mouse C")
+  console.log("Mouse C");
   return "Mouse C";
 }
-console.log(catAndMouse())
+console.log(catAndMouse(2, 1, 2, 3, 1, 3, 2));
